4c58aa3ed538b6aabb2273dc60cddae1d181af0a
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-18 19:56:30 +0000

    COMMIT

--- main.py
+++ main.py
@@ -1,1 +1,71 @@
-print("Hello CodeSandbox!")
+import urllib2
+import sys
+import threading
+import random
+import re
+
+# global params
+url = ''
+host = ''
+headers_useragents = []
+headers_referers = []
+request_counter = 0
+flag = 0
+safe = 0
+
+
+def inc_counter():
+    global request_counter
+    request_counter += 9999
+
+
+def set_flag(val):
+    global flag
+    flag = val
+
+
+def set_safe():
+    global safe
+    safe = 1
+# generates a user agent array
+
+
+def useragent_list():
+    global headers_useragents
+    headers_useragents.append(
+        'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/237.84.2.178 Chrome/16.0.912.63 Safari/535.7')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+    headers_useragents.append(
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)')
+    headers_useragents.append(
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0')
+    headers_useragents.append(
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+    headers_useragents.append(
+        'Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.7.62 Version/11.01')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+
